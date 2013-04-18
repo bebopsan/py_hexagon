11cb73805c90666d40023bc0e882cb5168cf13f0
Santiago <santiag77e@gmail.com>
2013-04-17 20:15:40 -0500

    Primer commit :) Añadí una librería que contiene definiciones de puntos rectangulos y cuadrados. Esta librería está diseñada para crear objetos que se puedan exportar a un programa llamado gmsh. Podemos modificarla para extraer de ahí clases útiles para el proyecto

---
+++ Lib/Point1.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+Code example from Think Python, by Allen B. Downey.
+Available from http://thinkpython.com
+
+Copyright 2012 Allen B. Downey.
+Distributed under the GNU General Public License at gnu.org/licenses/gpl.html.
+
+"""
+
+class Point(object):
+    """Represents a point in 2-D space."""
+    def __init__(self, id_tag, phys_tag, x=0, y = 0):
+        self.x = x
+        self.y = y
+        self.tag = tag
+        self.id_tag= 
+ 
+def print_point(p):
+    """Print a Point object in human-readable format."""
+    print '(%g, %g)' % (p.x, p.y)
+
+
+class Rectangle(object):
+    """Represents a rectangle. 
+
+    attributes: width, height, corner.
+    """
+
+
+    def find_center(self):
+        """Returns a Point at the center of a Rectangle."""
+        p = Point()
+        p.x = self.corner.x + self.width/2.0
+        p.y = self.corner.y + self.height/2.0
+        return p
+
+
+def grow_rectangle(rect, dwidth, dheight):
+    """Modify the Rectangle by adding to its width and height.
+
+    rect: Rectangle object.
+    dwidth: change in width (can be negative).
+    dheight: change in height (can be negative).
+    """
+    rect.width += dwidth
+    rect.height += dheight
+
--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 py_hexagon
 ==========
 
-Proyecto de programación del semillero de Física Teórica y Computacional de la Universidad EAFIT. 
+Proyecto de programación del semillero de Física Teórica y Computacional de la Universidad EAFIT.
+
+En esta carpeta alojaremos el código fuente del programa que iremos desarrollando como parte del proyecto del semillero.
+
+El programa py_hexagon* esta pensado para ser un clon del jego "Hexagon":
+ 
+http://www.kongregate.com/games/terrycavanagh_b/hexagon
+
+Y con el esperamos fortalecer las habilidades de programación y diseño de software de los estudiantes asociados al semillero.
+
+Inicialmente tendremos dos carpetas, una en la que alojaremos documentación, y otra en la cual estarán los archivos fuente, imágenes y ejecutables. Experimentaremos con una estructura similar a esta:
+
+Doc
+|-- tareas.txt: Roles y tareas pendientes por desarrollar. 
+|-- Referencias
+|        |---  Libros de programación y otras cosas...
+	
+Lib
+|--- clases.py:  definición de clases
+|--- engine.py:  define la ejecución del juego
+....
+
+
+* Me tomé la libertad de ponerle este nombre pero podemos ponerle cualquiera
+ 
